b801d88d2d2203947aa59ad62802903e267ae4cb
jayanarasimha275 <jayanarasimmha232@gmail.com>
2026-07-20 17:20:06 +0530

    Debug login route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,6 +5,13 @@ import { register, login } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/register", register);
-router.post("/login", login);
+router.post(
+  "/login",
+  (req, res, next) => {
+    console.log("✅ LOGIN ROUTE HIT");
+    next();
+  },
+  login,
+);
 
 export default router;
